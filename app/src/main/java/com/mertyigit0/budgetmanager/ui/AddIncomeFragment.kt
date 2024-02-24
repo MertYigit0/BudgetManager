1b1d2a65661aac7972d7da42628ae55965128bce
merty <mertyigit148@gmail.com>
2024-02-25 02:14:49 +0300

    Toggle Button

--- app/src/main/java/com/mertyigit0/budgetmanager/ui/AddIncomeFragment.kt
+++ app/src/main/java/com/mertyigit0/budgetmanager/ui/AddIncomeFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mertyigit0.budgetmanager.R
+import com.mertyigit0.budgetmanager.databinding.FragmentAddIncomeBinding
+import com.mertyigit0.budgetmanager.databinding.FragmentIncomeBinding
 
 
 class AddIncomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
+    private var _binding: FragmentAddIncomeBinding? = null;
+    private val binding get() = _binding!!;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +22,18 @@ class AddIncomeFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_income, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentAddIncomeBinding.inflate(inflater,container,false)
+        val view = binding.root;
+        return view
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 
 }
--- app/src/main/java/com/mertyigit0/budgetmanager/ui/IncomeFragment.kt
+++ app/src/main/java/com/mertyigit0/budgetmanager/ui/IncomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mertyigit0.budgetmanager.R
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
@@ -57,6 +58,16 @@ class IncomeFragment : Fragment() {
 
         // Chart'ın güncellenmesini sağla
         incomePieChart.invalidate()
+
+
+        binding.addIncomeButton.setOnClickListener{
+            val navController = findNavController()
+            navController.navigate(R.id.addIncomeFragment)
+        }
+
+
+
+
     }
 
 }

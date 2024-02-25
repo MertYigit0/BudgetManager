a58a2389dfd7d7c5f4cfb6ebe170aa719f5b93b9
merty <mertyigit148@gmail.com>
2024-02-25 18:21:05 +0300

    Toggle Group Buttons

--- app/src/main/java/com/mertyigit0/budgetmanager/ui/AddIncomeFragment.kt
+++ app/src/main/java/com/mertyigit0/budgetmanager/ui/AddIncomeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.button.MaterialButtonToggleGroup
+import com.google.android.material.snackbar.Snackbar
 import com.mertyigit0.budgetmanager.R
 import com.mertyigit0.budgetmanager.databinding.FragmentAddIncomeBinding
 import com.mertyigit0.budgetmanager.databinding.FragmentIncomeBinding
@@ -14,6 +18,8 @@ class AddIncomeFragment : Fragment() {
 
     private var _binding: FragmentAddIncomeBinding? = null;
     private val binding get() = _binding!!;
+
+    private lateinit var toggleButtonGroup: MaterialButtonToggleGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +39,36 @@ class AddIncomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        toggleButtonGroup = view.findViewById(R.id.toggleButtonGroup)
 
+        setupToggleButtonGroup()
+        }
+
+
+    private fun setupToggleButtonGroup() {
+        toggleButtonGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            val checkedButton = group.findViewById<MaterialButton>(checkedId)
+            if (isChecked) {
+                showSnackbar(checkedButton.text.toString())
+                uncheckOtherButtons(group, checkedId)
+            }
+        }
+    }
+
+    private fun uncheckOtherButtons(group: MaterialButtonToggleGroup, checkedId: Int) {
+        for (button in group.children) {
+            if (button.id != checkedId) {
+                group.uncheck(button.id)
+            }
+        }
+    }
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }

25cca4e1b62a628dd7b2b04442691d9759ca2a7f
merty <mertyigit148@gmail.com>
2024-04-24 01:42:57 +0300

    conflict fix2

--- app/src/main/java/com/mertyigit0/budgetmanager/SplashScreenActivity.kt
+++ app/src/main/java/com/mertyigit0/budgetmanager/SplashScreenActivity.kt
@@ -30,7 +30,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
         Handler().postDelayed({
             // Giriş sayısını kontrol ederek yönlendirme yapma
-            if (entryCount >= 99) {
+            if (entryCount >= 120) {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish() // SplashActivity'yi kapat
